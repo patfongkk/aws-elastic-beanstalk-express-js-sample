f2cdadbbdab0bd9a247cdf5791a13920aa84f8bd
pat fong <patfong@example.com>
2023-04-19 02:21:56 +0000

    Changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing 1 2 3....'));
+app.get('/', (req, res) => res.send('Good morning! Time for a break!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
