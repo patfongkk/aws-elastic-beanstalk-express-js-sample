ad2ff6486c9539522eeda066515e24c61a9ef555
patfongkk <43199378+patfongkk@users.noreply.github.com>
2025-05-20 12:54:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Coffee break! 15mins and let's do Lab 3'));
+app.get('/', (req, res) => res.send('Coffee break! 15mins and lets do Lab 3'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
